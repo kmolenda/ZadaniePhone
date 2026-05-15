49b0bcc73fb5c70853aca204a059aafe00de23a2
Krzysztof Molenda <kmolenda@wsei.edu.pl>
2026-05-15 13:58:53 +0200

    Add test project and integrate ClassLibrary with ConsoleApp
    Added TestProject with MSTest setup and parallel execution. Updated ConsoleApp to reference and use ClassLibrary's Phone class. Included basic test class and updated solution file.

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using ClassLibrary;
+using System;
+
+var telefon = new Phone("K. Molenda", "123456789");
+
+telefon.AddContact("Nowak", "987654321");
+
+Console.WriteLine( telefon.Call("Nowak") );
+
